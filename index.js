52bbec890002ce0e7fb306e31d1940f2b88cdec7
Moises Nitro 5 <moisesfonteles18@gmail.com>
2024-04-29 18:15:43 -0300

    removido rota 3

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ app.get("/rota2", function(req, res) {
     res.send("<html><head>Rota 2</head><body><h1>Rota 2</h1><p>Esta é a rota 2</p></body></html>")
 })
 
-app.get("/rota3", function(req, res) {
-    res.send("<html><head>Rota 3</head><body><h1>Rota 3</h1><p>Esta é a rota 3</p></body></html>")
-})
-
 app.listen(4000, () => {
     console.log('Servidor rodando em http://localhost:4000');
   });
